783252e93cbda08b99360ce16764fa05ef518945
schmidt-aaron <aschmidt808@gmail.com>
2020-01-18 21:27:41 -0800

    minor troubleshooting

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// module.exports = {
+//   parser: "babel-eslint",
+//   extends: ["airbnb", "prettier"],
+//   plugins: ["prettier", "jsx-a11y", "react", "react-hooks"],
+//   rules: {
+//     "prettier/prettier": ["error"],
+//     "comma-dangle": "off",
+//     "no-console": 1,
+//     "no-undef": "warn",
+//   },
+//   env: {
+//     browser: true,
+//     node: true,
+//     es6: true,
+//   },
+// };
